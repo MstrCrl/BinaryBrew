37ba4c6f2173f17744a77f1f22a20e9b452926b3
Aeri <victoriacarlangelo@gmail.com>
2024-12-07 09:30:01 +0800

    sign-in done master

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  signInForm: FormGroup; // Correct form name
+  type = false; // Toggle password visibility
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.signInForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  ngOnInit() {}
+
+  // Toggle password visibility
+  changeType() {
+    this.type = !this.type;
+  }
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      const { email, password } = this.signInForm.value;
+      console.log('Sign-In Data:', { email, password });
+    } else {
+      this.signInForm.markAllAsTouched();
+      console.log('Form is invalid');
+    }
+  }
 }
